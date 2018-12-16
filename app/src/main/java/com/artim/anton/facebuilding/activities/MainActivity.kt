728b73be65810606d73b2a0813cd8b42b1c8297e
Anton <artim-anton@yandex.ru>
2018-12-16 21:34:15 +0200

    Initial commit

---
+++ app/src/main/java/com/artim/anton/facebuilding/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.artim.anton.facebuilding.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.artim.anton.facebuilding.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
