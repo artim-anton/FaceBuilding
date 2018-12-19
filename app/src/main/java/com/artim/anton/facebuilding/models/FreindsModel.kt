dc6efa82f9a219e9a42d0c2b73f845e66f1a2f35
Anton <artim-anton@yandex.ru>
2018-12-19 07:57:32 +0200

    add FreindsModel

--- app/src/main/java/com/artim/anton/facebuilding/models/FreindsModel.kt
+++ app/src/main/java/com/artim/anton/facebuilding/models/FreindsModel.kt
@@ -1,7 +1,22 @@
 package com.artim.anton.facebuilding.models
 
 /**
- * Created by Artim Anton on 17.12.2018.
+ * Created by artim-anton on 17.12.2018.
  */
 class FreindsModel {
+
+    private var name: String
+    private var surname: String
+    private var city: String?
+    private var avatar: String?
+    private var isOnline: Boolean
+
+    constructor(name: String, surname: String, city: String?, avatar: String?, isOnline: Boolean){
+        this.name = name
+        this.surname = surname
+        this.city = city
+        this.avatar = avatar
+        this.isOnline = isOnline
+
+    }
 }
